529f355154a6f57663ce3aa0cb955f79da0aedf4
Tamia.o <mia.ogletree13@gmail.com>
2022-01-06 14:41:40 -0500

    v0.3
    Added color values for WHITE, RED, GREEN, and BLUE

--- Simple_animation.py
+++ Simple_animation.py
@@ -1,6 +1,7 @@
-#Simple Animation with PyGame, Tamia Ogletree, 01-6-22, 2:29pm, v0.2
+#Simple Animation with PyGame, Tamia Ogletree, 01-6-22, 2:39pm, v0.3
  
- import pygame,sys, time
+ from _typeshed import ReadableBuffer
+import pygame,sys, time
  from pygame.locals import
 
  # Setup Pygame 
@@ -19,3 +20,9 @@ UPLEFT = 'upleft'
 UPRIGHT = 'upright'
 
 MOVESPEED = 4
+
+# Setupcolor values.
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255,0)
+BLUE = (0, 0, 255)
