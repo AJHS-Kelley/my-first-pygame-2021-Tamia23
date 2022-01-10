9a29361985f8770f479be8fae2655bc010d60081
Tamia.o <mia.ogletree13@gmail.com>
2022-01-10 14:53:01 -0500

    v0.7
     not finished

--- Simple_animation.py
+++ Simple_animation.py
@@ -57,3 +57,25 @@ while True:
                 if b ['dir'] == UPRIGHT:
                     b['rect'].left += MOVESPEED
                     b['rect'].top -= MOVESPEED
+
+            if b['rect'].top < 0:
+                # The box has moved past the top.
+                if b['rect'] == UPLEFT:
+                    b['dir'] = DOWNLEFT
+                if b['dir'] == UPRIGHT:
+                    b['dir'] = DOWNRIGHT
+                if b['rect'].bottom > WINDOWHEIGHT:
+                    # The box has moved past the bottom 
+                    if b['dir'] == DOWNLEFT:
+                        b['dir'] = UPLEFT
+                    if b['dir'] == DOWNRIGHT:
+                        b['dir'] = UPRIGHT 
+                    if b['rect'] < 0:
+                        # The box has moved past the left.
+                    if b['dir'] == DOWNLEF:
+                        b['dir'] = DOWNRIGHT 
+                    if b['dir'] == UPLEFT:
+                    b['dir'] = UPRIGHT
+                if b['rect'].right > WINDOWWIDTH: 
+                    # The box has moved past the right.
+                if
